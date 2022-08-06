27425dce5ec57531083f5dcddd05001a051df2e4
rabuhassan <103109080+rabuhassan@users.noreply.github.com>
2022-08-06 19:48:42 +0200

    Completed customer analysis and clustering.

--- LSE_DA301_assignment_JupyterNotebook_Rachid_Abu-Hassan.py
+++ LSE_DA301_assignment_JupyterNotebook_Rachid_Abu-Hassan.py
@@ -348,6 +348,318 @@ plt.show()
 #   * Loyalty Points and Spending Score = r-sqrd is 0.452
 
 # ## 2. How groups within the customer base can be used to target specific market segments?
+# Examine the usefulness of remuneration and spending scores in providing data for analysis. Identify groups within the customer base that can be used to target specific market segments. 
+
+# ### 2.1 Workstation Setup (additional)
+
+# In[110]:
+
+
+# Import Additional Necessary Packages.
+import matplotlib.cm as cm
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import accuracy_score
+from scipy.spatial.distance import cdist
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# In[125]:
+
+
+# Load the 'reviews_clean' database and create the DataFrame df2. 
+df2 = pd.read_csv('reviews_clean.csv', 
+                  index_col=None)
+
+
+# ### 2.2 Explore the Data and Reduce the DataFrame to Necessary Columns
+
+# In[126]:
+
+
+# View the DataFrame. 
+df2.head()
+
+
+# In[127]:
+
+
+# Drop all columns except 'salary' and 'spending_score'.
+df2.drop(['Unnamed: 0', 'gender', 'age', 'loyalty_points', 'education', 'product', 'review', 'summary'], 
+             axis=1, 
+             inplace=True)
+df2.head()
+
+
+# In[128]:
+
+
+df2.info()
+
+
+# In[129]:
+
+
+# Descriptive statistics.
+df2.describe()
+
+
+# ### 2.3 Create Initial Plots
+
+# In[131]:
+
+
+# Create a scatterplot with Seaborn.
+sns.scatterplot(x='salary', 
+                y='spending_score', 
+                data=df2)
+
+
+# In[147]:
+
+
+# Create a pairplot with Seaborn.
+x = df2[['salary', 'spending_score']]
+
+sns.pairplot(df2, 
+             vars=x, 
+             diag_kind= 'kde')
+
+
+# ### 2.4 Determine the Number of Clusters
+
+# #### 2.41 Elbow Method
+
+# In[148]:
+
+
+# Elbow chart to decide on the number of optimal clusters.
+cs = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', 
+                    max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(x)
+    cs.append(kmeans.inertia_)
+
+plt.plot(range(1, 11), cs, marker='o')
+plt.title("The Elbow Method")
+plt.xlabel("Number of clusters")
+plt.ylabel("CS")
+
+plt.show()
+
+
+# #### 2.42 Silhoutte Method
+
+# In[149]:
+
+
+# Find the range of clusters to be used using silhouette method.
+sil = []
+kmax = 10
+
+for k in range(2, kmax+1):
+    kmeans_s = KMeans(n_clusters = k).fit(x)
+    labels = kmeans_s.labels_
+    sil.append(silhouette_score(x, labels, metric = 'euclidean'))
+
+# Plot the silhouette method.
+plt.plot(range(2, kmax+1), sil, marker='o')
+
+plt.title("The Silhouette Method")
+plt.xlabel("Number of clusters")
+plt.ylabel("Sil")
+
+plt.show()
+
+
+# #### 2.43 Conclusion
+# Evaluate clusters is **4, 5, and 6**; although, it is clear from the Elbow Method that 5 clusters is best. 
+
+# ### 2.5 Evaluate k-means model at 4, 5, and 6
+
+# #### 2.51 Evaluate and Fit 4 Clusters
+
+# In[197]:
+
+
+# Use 4 clusters:
+kmeans = KMeans(n_clusters = 4, max_iter = 15000, init='k-means++', random_state=0).fit(x)
+clusters = kmeans.labels_
+x['K-Means Predicted'] = clusters
+
+# Plot the predicted.
+sns.pairplot(x, hue='K-Means Predicted', diag_kind= 'kde')
+
+
+# In[198]:
+
+
+# Check the number of observations per predicted class.
+x['K-Means Predicted'].value_counts()
+
+
+# In[199]:
+
+
+# View the K-Means predicted.
+print(x.head())
+
+
+# In[200]:
+
+
+# Visualising the clusters.
+# Set plot size.
+sns.set(rc = {'figure.figsize':(12, 8)})
+
+sns.scatterplot(x='salary' , 
+                y='spending_score',
+                data=x , hue='K-Means Predicted',
+                palette=['red', 'green', 'blue', 'orange'])
+
+
+# #### 2.52 Evaluate and Fit 5 Clusters
+
+# In[201]:
+
+
+# Use 5 clusters:
+kmeans = KMeans(n_clusters = 5, max_iter = 15000, init='k-means++', random_state=0).fit(x)
+clusters = kmeans.labels_
+x['K-Means Predicted'] = clusters
+
+# Plot the predicted.
+sns.pairplot(x, hue='K-Means Predicted', diag_kind= 'kde')
+
+
+# In[202]:
+
+
+# Check the number of observations per predicted class.
+x['K-Means Predicted'].value_counts()
+
+
+# In[203]:
+
+
+# View the K-Means predicted.
+print(x.head())
+
+
+# In[204]:
+
+
+# Visualising the clusters.
+# Set plot size.
+sns.set(rc = {'figure.figsize':(12, 8)})
+
+sns.scatterplot(x='salary' , 
+                y='spending_score',
+                data=x , hue='K-Means Predicted',
+                palette=['red', 'green', 'blue', 'orange', 'brown'])
+
+
+# #### 2.53 Evaluate and Fit 6 Clusters
+
+# In[205]:
+
+
+# Use 6 clusters:
+kmeans = KMeans(n_clusters = 6, max_iter = 15000, init='k-means++', random_state=0).fit(x)
+clusters = kmeans.labels_
+x['K-Means Predicted'] = clusters
+
+# Plot the predicted.
+sns.pairplot(x, hue='K-Means Predicted', diag_kind= 'kde')
+
+
+# In[206]:
+
+
+# Check the number of observations per predicted class.
+x['K-Means Predicted'].value_counts()
+
+
+# In[207]:
+
+
+# View the K-Means predicted.
+print(x.head())
+
+
+# In[208]:
+
+
+# Visualising the clusters.
+# Set plot size.
+sns.set(rc = {'figure.figsize':(12, 8)})
+
+sns.scatterplot(x='salary' , 
+                y='spending_score',
+                data=x , hue='K-Means Predicted',
+                palette=['red', 'green', 'blue', 'orange', 'brown', 'yellow'])
+
+
+# ### 2.6 Fit the Final Model (k=5)
+
+# In[209]:
+
+
+# Use 5 clusters:
+kmeans = KMeans(n_clusters = 5, max_iter = 15000, init='k-means++', random_state=0).fit(x)
+clusters = kmeans.labels_
+x['K-Means Predicted'] = clusters
+
+# Plot the predicted.
+sns.pairplot(x, hue='K-Means Predicted', diag_kind= 'kde')
+
+
+# In[210]:
+
+
+# Check the number of observations per predicted class.
+x['K-Means Predicted'].value_counts()
+
+
+# In[211]:
+
+
+# View the K-Means predicted.
+print(x.head())
+
+
+# In[212]:
+
+
+# Visualising the clusters.
+# Set plot size.
+sns.set(rc = {'figure.figsize':(12, 8)})
+
+sns.scatterplot(x='salary' , 
+                y='spending_score',
+                data=x , hue='K-Means Predicted',
+                palette=['red', 'green', 'blue', 'orange', 'brown'])
+
+
+# ### 2.6 Observations and Insights
+# 
+# Based on the Elbow and Silhoutte Methods, it was clear that 5 clusters are best for the dataset considered. That was also visually confirmed early on by using a scatterplot of the salary and spending_score data. The plot was already showing more or less 5 clear clusters. 
+# 
+# Nonetheless, for good measure, 3 different cluster (k-values) were evaluated; 4, 5, and 6 clusters. Further, the all cluster values were evaluated, fitted, and ploted. This, further confirmed that 5 clusters is best for the dataset. 
+# 
+# The 5 clusters consist of the following numbers: 
+# * 0    356
+# * 1    774
+# * 2    330
+# * 3    269
+# * 4    271
+# 
+# Clearly, cluster 1 is the largers with almost double any other cluster. Salaries within this cluster raged from 30k to 55k, while the spending score ranges from 40 to 60. 
 
 # In[ ]:
 
